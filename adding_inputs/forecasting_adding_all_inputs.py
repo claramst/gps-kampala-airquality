Remove outliers for every model in training set. Corrected way of averaging rmse

adding_inputs/forecasting_adding_all_inputs.py
@@ -16,20 +16,20 @@ import tensorflow as tf
 tf.config.threading.set_inter_op_parallelism_threads(1)
 tf.config.threading.set_intra_op_parallelism_threads(1)
 
-nov_df = pd.read_csv('nov-data.csv')
+df = pd.read_csv('nov-data.csv')
 weather_df = pd.read_csv('weather-data.csv')
 
-nov_df_no_outliers = pd.DataFrame()
-
-for site in nov_df.site_id.unique():
-  site_df = nov_df[nov_df['site_id']==site]
-  Q1 = site_df['pm2_5_calibrated_value'].quantile(0.25)
-  Q3 = site_df['pm2_5_calibrated_value'].quantile(0.75)
-  IQR = Q3 - Q1
-  final_df = site_df[~((site_df['pm2_5_calibrated_value']<(Q1-1.5*IQR)) | (site_df['pm2_5_calibrated_value']>(Q3+1.5*IQR)))]
-  nov_df_no_outliers = pd.concat([nov_df_no_outliers, final_df], ignore_index=True)
-
-df = nov_df_no_outliers
+# nov_df_no_outliers = pd.DataFrame()
+#
+# for site in nov_df.site_id.unique():
+#   site_df = nov_df[nov_df['site_id']==site]
+#   Q1 = site_df['pm2_5_calibrated_value'].quantile(0.25)
+#   Q3 = site_df['pm2_5_calibrated_value'].quantile(0.75)
+#   IQR = Q3 - Q1
+#   final_df = site_df[~((site_df['pm2_5_calibrated_value']<(Q1-1.5*IQR)) | (site_df['pm2_5_calibrated_value']>(Q3+1.5*IQR)))]
+#   nov_df_no_outliers = pd.concat([nov_df_no_outliers, final_df], ignore_index=True)
+#
+# df = nov_df_no_outliers
 
 sites = df.site_id.unique()
 print("Number of sites:")
@@ -119,16 +119,27 @@ print(last_hour.site_id.unique().shape)
 
 # #### Forecasting last hour
 
-train = df.drop(last_hour.index)
+train = df.drop(last_day.index)
+train_no_outliers = pd.DataFrame()
+
+for site in train.site_id.unique():
+    site_df = train[train['site_id']==site]
+    Q1 = site_df['pm2_5_calibrated_value'].quantile(0.25)
+    Q3 = site_df['pm2_5_calibrated_value'].quantile(0.75)
+    IQR = Q3 - Q1
+    final_df = site_df[~((site_df['pm2_5_calibrated_value']<(Q1-1.5*IQR)) | (site_df['pm2_5_calibrated_value']>(Q3+1.5*IQR)))]
+    train_no_outliers = pd.concat([train_no_outliers, final_df], ignore_index=True)
+
+train = train_no_outliers
 
 
 def train_test_forecast_hour_gp(df, site_id, kernel, input_features):
     test = df.loc[last_day.index]
     test = test[test['site_id'] == site_id]
 
-    mses = np.zeros(3)
+    mses = np.zeros(4)
 
-    for i in range(3):
+    for i in range(4):
         if len(test) == 0:
             return 0
         if len(test) > 250:
@@ -227,7 +238,7 @@ for i in range(0, len(sites)):
 
 fm_hour = forecast_hour_mses[forecast_hour_mses != 0]
 
-avg_rmse = np.sqrt(np.average(fm_hour))
+avg_rmse = np.average(np.sqrt(fm_hour))
 max_rmse = np.sqrt(np.max(fm_hour))
 min_rmse = np.sqrt(np.min(fm_hour))
 print(min_rmse)

adding_inputs/nowcasting_adding_all_inputs.py
@@ -29,7 +29,8 @@ for site in nov_df.site_id.unique():
   final_df = site_df[~((site_df['pm2_5_calibrated_value']<(Q1-1.5*IQR)) | (site_df['pm2_5_calibrated_value']>(Q3+1.5*IQR)))]
   nov_df_no_outliers = pd.concat([nov_df_no_outliers, final_df], ignore_index=True)
 
-df = nov_df_no_outliers
+df_no_outliers = nov_df_no_outliers
+df = nov_df
 
 sites = df.site_id.unique()
 print("Number of sites:")
@@ -111,12 +112,11 @@ mean_cloud_cover = df['cloudcover'].mean(axis=0)
 std_cloud_cover = df['cloudcover'].std(axis=0)
 
 def train_test_gp(df, site_id, kernel, input_features):
-    mses = np.zeros((3))
-    # train = get_closest_data(df, site_id)
+    mses = np.zeros((4))
     test = df[df['site_id']==site_id]
-    train = df.drop(test.index)
+    train = df_no_outliers[df_no_outliers['site_id'] != site_id]
 
-    for i in range(3):
+    for i in range(4):
         if len(test) == 0:
             return 0
         if len(test) > 250:
@@ -210,7 +210,7 @@ for i in range(0, len(sites)):
     mse = train_test_gp(df, sites[i], periodic_kernel, input_features)
     periodic_mses[i] = mse
 
-avg_rmse = np.sqrt(np.average(periodic_mses))
+avg_rmse = np.average(np.sqrt(periodic_mses))
 max_rmse = np.sqrt(np.max(periodic_mses))
 min_rmse = np.sqrt(np.min(periodic_mses))
 print(min_rmse)

no_outliers/forecasting_periodic_no_outliers.py
@@ -76,15 +76,11 @@ def train_test_forecast_gp(df, site_id, kernel):
     test = df.loc[last_day.index]
     test = test[test['site_id'] == site_id]
 
-    mses = np.zeros(3)
+    mses = np.zeros(4)
 
-    for i in range(3):
+    for i in range(4):
       if len(test) == 0:
         return 0
-      if len(test) > 250:
-          rand_test = test.sample(n=250, random_state=i)
-      else:
-          rand_test = test
 
       if len(train) > 1000:
           rand_train = train.sample(n=1000, random_state=i)
@@ -140,7 +136,7 @@ for i in range(0, len(sites)):
 
 fm_hour = forecast_hour_mses[forecast_hour_mses != 0]
 
-avg_rmse = np.sqrt(np.average(fm_hour))
+avg_rmse = np.average(np.sqrt(periodic_mses))
 max_rmse = np.sqrt(np.max(fm_hour))
 min_rmse = np.sqrt(np.min(fm_hour))
 print(min_rmse)

no_outliers/nowcasting_periodic_no_outliers.py
@@ -67,15 +67,11 @@ mean_longitude = df['longitude'].mean(axis=0)
 std_longitude = df['longitude'].std(axis=0)
 
 def train_test_gp(df, site_id, kernel):
-    mses = np.zeros((3))
+    mses = np.zeros((4))
     test = df[df['site_id']==site_id]
     train = df_no_outliers[df_no_outliers['site_id'] != site_id]
 
-    for i in range(3):
-      if len(test) > 250:
-          rand_test = test.sample(n=250, random_state=i)
-      else:
-          rand_test = test
+    for i in range(4):
 
       if len(train) > 1000:
           rand_train = train.sample(n=1000, random_state=i)
@@ -133,7 +129,7 @@ for i in range(0, len(sites)):
     mse = train_test_gp(df, sites[i], periodic_kernel)
     periodic_mses[i] = mse
 
-avg_rmse = np.sqrt(np.average(periodic_mses))
+avg_rmse = np.average(np.sqrt(periodic_mses))
 max_rmse = np.sqrt(np.max(periodic_mses))
 min_rmse = np.sqrt(np.min(periodic_mses))
 print(min_rmse)

nowcasting/nowcasting.py
@@ -88,11 +88,6 @@ def train_test_gp(df, site_id, kernel):
     train = df.drop(test.index)
 
     for i in range(4):
-      if len(test) > 250:
-          rand_test = test.sample(n=250, random_state=i)
-      else:
-          rand_test = test
-
       if len(train) > 1000:
           rand_train = train.sample(n=1000, random_state=i)
       else:
@@ -157,7 +152,7 @@ for i in range(0, len(sites)):
     mse = train_test_gp(df, sites[i], kernel)
     mses[i] = mse
 
-avg_rmse = np.sqrt(np.average(mses))
+avg_rmse = np.average(np.sqrt(mses))
 max_rmse = np.sqrt(np.max(mses))
 min_rmse = np.sqrt(np.min(mses))
 print(min_rmse)

sparse_approximations/sparse_gp.py
@@ -20,19 +20,18 @@ tf.config.threading.set_intra_op_parallelism_threads(1)
 nov_df = pd.read_csv('nov-data.csv')
 weather_df = pd.read_csv('weather-data.csv')
 
-# nov_df_no_outliers = pd.DataFrame()
-#
-# for site in nov_df.site_id.unique():
-#   site_df = nov_df[nov_df['site_id']==site]
-#   Q1 = site_df['pm2_5_calibrated_value'].quantile(0.25)
-#   Q3 = site_df['pm2_5_calibrated_value'].quantile(0.75)
-#   IQR = Q3 - Q1
-#   final_df = site_df[~((site_df['pm2_5_calibrated_value']<(Q1-1.5*IQR)) | (site_df['pm2_5_calibrated_value']>(Q3+1.5*IQR)))]
-#   nov_df_no_outliers = pd.concat([nov_df_no_outliers, final_df], ignore_index=True)
-#
-# df = nov_df
-# df_no_outliers = nov_df_no_outliers
+nov_df_no_outliers = pd.DataFrame()
+
+for site in nov_df.site_id.unique():
+  site_df = nov_df[nov_df['site_id']==site]
+  Q1 = site_df['pm2_5_calibrated_value'].quantile(0.25)
+  Q3 = site_df['pm2_5_calibrated_value'].quantile(0.75)
+  IQR = Q3 - Q1
+  final_df = site_df[~((site_df['pm2_5_calibrated_value']<(Q1-1.5*IQR)) | (site_df['pm2_5_calibrated_value']>(Q3+1.5*IQR)))]
+  nov_df_no_outliers = pd.concat([nov_df_no_outliers, final_df], ignore_index=True)
+
 df = nov_df
+df_no_outliers = nov_df_no_outliers
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--site_index', type=int, default=0, help='selects site')
@@ -76,17 +75,14 @@ df = df.drop(['windgust', 'datetime'], axis=1)
 
 if forecasting:
     last_day = df[df['Day'].astype(str)=='2021-11-30']
-    last_hour = last_day[last_day['IndexTime']==23]
-    train = df.drop(last_hour.index)
-    test = df.loc[last_hour.index]
-    test = test[test['site_id'] == site_id]
+    test = df.loc[last_day.index]
+    test = test[test['site_id'] == test_site]
+    train = df_no_outliers[df_no_outliers['Day'].astype(str) !='2021-11-30']
     if len(test) == 0:
         sys.exit("Site has no readings at forecast test time")
 else:
     test = df[df['site_id']==test_site]
-    train = df.drop(test.index)
-    train = train.sample(n=50, random_state=0)
-
+    train = df_no_outliers[df_no_outliers['site_id'] != test_site]
 
 """
 

sparse_approximations/st_svgp.py
@@ -12,7 +12,15 @@ from jax.lib import xla_bridge
 
 df = pd.read_csv('nov-data.csv')
 
-# nov_df_no_outliers = pd.DataFrame()
+df_no_outliers = pd.DataFrame()
+
+for site in nov_df.site_id.unique():
+  site_df = df[df['site_id']==site]
+  Q1 = site_df['pm2_5_calibrated_value'].quantile(0.25)
+  Q3 = site_df['pm2_5_calibrated_value'].quantile(0.75)
+  IQR = Q3 - Q1
+  final_df = site_df[~((site_df['pm2_5_calibrated_value']<(Q1-1.5*IQR)) | (site_df['pm2_5_calibrated_value']>(Q3+1.5*IQR)))]
+  df_no_outliers = pd.concat([df_no_outliers, final_df], ignore_index=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--site_index', type=int, default=0, help='selects site')
@@ -113,13 +121,16 @@ Y = Y[unique_idx, :]
 # For the filtering methods to work we need a full spatio-temporal grid
 X_raw, Y_raw = pad_with_nan_to_make_grid(X.copy(), Y.copy())
 
-
-# if forecasting:
-#     ### TODO
-# else:
-test = df[df['site_id']==test_site]
-train = df.drop(test.index)
-train = train.sample(n=50, random_state=0)
+if forecasting:
+    last_day = df[df['Day'].astype(str)=='2021-11-30']
+    test = df.loc[last_day.index]
+    test = test[test['site_id'] == test_site]
+    train = df_no_outliers[df_no_outliers['Day'].astype(str) !='2021-11-30']
+    if len(test) == 0:
+        sys.exit("Site has no readings at forecast test time")
+else:
+    test = df[df['site_id']==test_site]
+    train = df_no_outliers[df_no_outliers['site_id'] != test_site]
 
 test_latitude = test.latitude.unique()[0]
 test_longitude = test.longitude.unique()[0]
@@ -139,16 +150,6 @@ Y_all = Y_raw
 X_train_norm = normalise_df(X_train, wrt_to=X_train)
 X_test_norm = normalise_df(X_test, wrt_to=X_train)
 X_all_norm = normalise_df(X_all, wrt_to=X_train)
-
-#Normalise Data input
-def normalise(x, wrt_to):
-    return (x - np.mean(wrt_to))/np.std(wrt_to)
-
-def normalise_df(x, wrt_to):
-    return (x - np.mean(wrt_to, axis=0))/np.std(wrt_to, axis=0)
-
-def un_normalise_df(x, wrt_to):
-    return x* np.std(wrt_to, axis=0) + np.mean(wrt_to, axis=0)
 
 X = X_train_norm
 Y = Y_train